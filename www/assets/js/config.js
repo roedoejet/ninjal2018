bb6d3010f580214e207ed9fdb50aa625d5ab9246
Aidan Pine <aidanpine@shaw.ca>
2018-08-10 14:56:56 +0900

    Add files via upload

--- www/assets/js/config.js
+++ www/assets/js/config.js
@@ -1,26 +1,27 @@
 
 
-waldayu.transducers["heiltsuk_compare"] = (function() {
- var correspondences = {'aa': 'a', 'ee': 'e', 'ii': 'i', "'n": 'n', 'from': 'to', 'g_': 'g', 'k\u0332': 'k', "k\u0331'": 'k', 'x_': 'x', "'w": 'w', 'g\u0332': 'g', "kw'": 'kw', "'y": 'y', "'a": 'a', 'x\u0332': 'x', "k'": 'k', "'m": 'm', "'l": 'l', 'k_': 'k', 'oo': 'o', 'uu': 'u', "ts'": 'ts', "p'": 'p', "t'": 't'};
- var keys = ['from', "k\u0331'", "kw'", "ts'", 'g\u0332', 'x\u0332', 'k\u0332', 'g_', 'x_', 'k_', 'aa', 'ii', 'ee', 'oo', 'uu', "t'", "k'", "p'", "'l", "'n", "'m", "'w", "'y", "'a"];
- var regex = new RegExp("(" + keys.join('|') + ")", 'g');
- return function(str) {
-     return str.replace(regex, function(a,b) {
-         return correspondences[a];
-     });
- };
-})();var config = {
- "L1": {
-     "name": "Heiltsuk",
-     "underlying": "",
-     "compare": "heiltsuk_compare",
-     "lettersInLanguage" : ['a', 'aa', 'b', 'd', 'e', 'ee', 'g', 'g\\u0331', 'gw', 'gy', 'h', 'hl', 'i', 'ii', 'j', 'k', 'k\\u0331', "k'", "k\\u0331'", 'kw', "kw'", 'ky', "ky'", 'l', "'l", 'm', "'m", 'n', "'n", 'o', 'oo', 'p', "p'", 's', 't', "t'", 'ts', "ts'", 'u', 'uu', 'w', "'w", 'x', 'x\\u0331', 'xw', 'y', "'y", "'"]
- },
- "L2": {
-     "name": "English",
-     "underlying": "",
-     "compare": ""
- }
+waldayu.transducers["gitksan_compare"] = (function() {
+    var correspondences = {'g̲': 'g', 'x̲': 'x', 'k̲': 'k', 'g_': 'g', 'x_': 'x', 'k_': 'k', 'aa': 'a', 'ii': 'i', 'ee': 'e', 'oo': 'o', 'uu': 'u', "t'": 't', "k'": 'k', "p'": 'p', "ḵ'": 'k', "kw'": 'kw', "ts'": 'ts', "'l": 'l', "'n": 'n', "'m": 'm', "'w": 'w', "'y": 'y'};
+    var keys = ["ḵ'", "kw'", "ts'", 'g̲', 'x̲', 'k̲', 'g_', 'x_', 'k_', 'aa', 'ii', 'ee', 'oo', 'uu', "t'", "k'", "p'", "'l", "'n", "'m", "'w", "'y"];
+    var regex = new RegExp("(" + keys.join('|') + ")", 'g');
+    return function(str) {
+        return str.replace(regex, function(a,b) {
+            return correspondences[a];
+        });
+    };
+})();
+var config = {
+    "L1": {
+        "name": "Gitksan",
+        "underlying": "",
+        "compare": "gitksan_compare",
+        "lettersInLanguage" : ['a', 'aa', 'b', 'd', 'e', 'ee', 'g', 'g̲', 'gw', 'gy', 'h', 'hl', 'i', 'ii', 'j', 'k', 'k̲', "k'", "k̲'", 'kw', "kw'", 'ky', "ky'", 'l', "'l", 'm', "'m", 'n', "'n", 'o', 'oo', 'p', "p'", 's', 't', "t'", 'ts', "ts'", 'u', 'uu', 'w', "'w", 'x', 'x̲', 'xw', 'y', "'y", "'"]
+    },
+    "L2": {
+        "name": "English",
+        "underlying": "",
+        "compare": ""
+    }
 };
 
 /* Search Variables */
